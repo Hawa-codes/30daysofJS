64c2630225b63132701a5a28c78a263d3405270b
Hawa-codes <adediranhawa@gmail.com>
2025-08-11 22:51:08 +0100

    Complete Day 1

---
+++ Day1/script.js
@@ -0,0 +1,48 @@
+/* variables 
+firstName = "Hawa"
+console.log(firstName) // Hawa  
+*/
+
+/* string 
+'a'
+'Asabeneh'
+"Asabeneh"
+'Finland'
+'JavaScript is a beautiful programming language'
+'I love teaching'
+'I hope you are enjoying the first day'
+*/
+
+/* boolean 
+true // if the light is on, the value is true
+false // if the light is off, the value is false
+*/
+
+/* integer 
+-3, -2, -1, 0, 1, 2, 3, -3.5, -2.25
+*/
+
+console.log(typeof 'Asabeneh') // string
+console.log(typeof 5) // number
+console.log(typeof true) // boolean
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined
+
+/* 
+firstName
+lastName
+country
+city
+capitalCity
+age
+isMarried
+*/
+
+// Exercise
+1. // comments can make code readable
+2. // Welcome to 30DaysOfJavaScript
+3. /* 
+comments can make code readable, easy to reuse and informative
+*/
+9. // let firstName = "Hawa", lastName = "Adediran", maritalStatus = "Single", country = "Nigeria", age = 22;
+console.log(firstName, lastName, maritalStatus, country, age);
---
+++ Day1/variable.js
@@ -0,0 +1,51 @@
+let city = 'Osun'         // capital city
+let isMarried = false // marital status
+
+let emptyValue = null // empty value
+
+let secondName
+console.log(secondName) // undefined
+
+
+// no 6
+let fName;
+let sName;        
+let lName;
+let cName;
+
+console.log(fName); // undefined
+console.log(sName); // undefined
+console.log(lName);  // undefined
+console.log(cName);   // undefined
+
+
+// no 7
+let fiName = "Hawa";
+let ciName = "Lagos";
+let laName = "Adediran";
+let coName = "Nigeria";
+
+console.log(fiName); // Hawa
+console.log(ciName); // Lagos
+console.log(laName); // Adediran
+console.log(coName); // Nigeria
+
+// no 8
+let firstName = "Hawa";
+let lastName = "Adediran";
+let maritalStatus = "Single";
+let country = "Nigeria";
+let age = 20;
+
+console.log(firstName);
+console.log(lastName);
+console.log(maritalStatus);
+console.log(country);
+console.log(age);
+
+// no 10
+let myAge = 22;
+let yourAge = 30;
+
+console.log("My age:", myAge);
+console.log("Your age:", yourAge);
