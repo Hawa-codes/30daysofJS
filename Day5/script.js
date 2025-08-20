11d4825cdd483d25fd622e8245581fc4f2c029a1
Hawa-codes <adediranhawa@gmail.com>
2025-08-21 00:00:22 +0100

    Day 5

--- Day5/script.js
+++ Day5/script.js
@@ -43,3 +43,48 @@ console.log("First company:", itCompanies[3]);
 console.log("First company:", itCompanies[4]);
 console.log("First company:", itCompanies[5]);
 console.log("First company:", itCompanies[6]);
+
+// no 11 Change each company name to uppercase one by one and print them out
+console.log(itCompanies[0].toUpperCase()); 
+console.log(itCompanies[1].toUpperCase()); 
+console.log(itCompanies[2].toUpperCase()); 
+console.log(itCompanies[3].toUpperCase()); 
+console.log(itCompanies[4].toUpperCase()); 
+console.log(itCompanies[5].toUpperCase()); 
+console.log(itCompanies[6].toUpperCase());
+
+// no 12 Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+let sentence = itCompanies.slice(0, -1).join(", ") + " and " + itCompanies[itCompanies.length - 1] + " are big IT companies.";
+console.log(sentence);  // Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon are big IT companies.
+
+// no 13 Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is not found
+let company = "Google"; 
+if (itCompanies.includes(company)) {
+  console.log(company);  
+} else {
+  console.log("Company is not found");
+}
+// no 14 Filter out companies which have more than one 'o' without the filter method
+// no 15 Sort the array using sort() method
+itCompanies.sort()
+console.log(itCompanies);
+
+// no 16 Reverse the array using reverse() method
+itCompanies.reverse()
+console.log(itCompanies);
+
+// no 17 Slice out the first 3 companies from the array
+console.log(itCompanies.slice(3,7));
+
+// no 18 Slice out the last 3 companies from the array
+console.log(itCompanies.slice(0,4));
+
+// no 19 Slice out the middle IT company or companies from the array
+console.log(itCompanies.slice(3,4)); 
+
+// no 20 Remove the first IT company from the array
+
+
+// no 21 Remove the middle IT company or companies from the array
+// no 22 Remove the last IT company from the array
+// no 22 Remove all IT companies
