8ac10b103f558373ccc656fab01ea9df44d797c1
Hawa-codes <adediranhawa@gmail.com>
2025-09-11 21:47:59 +0100

    Day 11

---
+++ Day11/script.js
@@ -0,0 +1,3 @@
+// Destructure and assign the elements of constants array to e, pi, gravity, humanBodyTemp, waterBoilingTemp.
+// Destructure and assign the elements of countries array to fin, est, sw, den, nor
+// Destructure the rectangle object by its properties or keys.
