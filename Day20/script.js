041eb9763c762aaa87177e14eac86205d0045b7d
Hawa-codes <adediranhawa@gmail.com>
2025-09-14 15:52:50 +0100

    Day 20 complete

---
+++ Day20/script.js
@@ -0,0 +1,143 @@
+// We used camelCase for variables and functions.
+// All variable names start with a letter.
+// We chose to use const for constants, arrays, objects and functions. In stead of double quote, we chose to use single quote or backtick. Single quote is becoming trendy.
+// We also removed semicolons from our code but it is a matter of personal preference.
+// Space around arithmetic operators, assignment operators and after comma
+// Arrow function instead of function declaration
+// Explicit return instead of implicit return if the function is one liner
+// No trailing comma in the last value of an object
+// We prefer this +=, -=, *= /=, **= instead of the longer version
+// When we use console.log() it is good to print with a tag string to identify from where the console is coming
+// variable
+let firstName = 'Asabeneh'
+let lastName = 'Yetayeh'
+let country = 'Finland'
+let city = 'Helsinki'
+
+const PI = Math.PI
+const gravity = 9.81
+
+// arrays
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const numbers = [0, 3.14, 9.81, 37, 98.6, 100]
+const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland']
+const languages = ['Amharic', 'Arabic', 'English', 'French', 'Spanish']
+const skills = ['HTML', 'CSS', 'JavaScript', 'React', 'Python']
+const fruits = ['banana', 'orange', 'mango', 'lemon']
+const vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
+
+// function which return full name of a person
+const printFullName = (firstName, lastName) => firstName + ' ' + lastName
+
+// function which calculates a square of a number
+const square = (n) => n * n
+
+// a function which generate random hexa colors
+const hexaColor = () => {
+  const str = '0123456789abcdef'
+  let hexa = '#'
+  let index
+  for (let i = 0; i < 6; i++) {
+    index = Math.floor(Math.random() * str.length)
+    hexa += str[index]
+  }
+  return hexa
+}
+
+// a function which shows date and time
+const showDateTime = () => {
+  const now = new Date()
+  const year = now.getFullYear()
+  const month = now.getMonth() + 1
+  const date = now.getDate()
+  let hours = now.getHours()
+  let minutes = now.getMinutes()
+  if (hours < 10) {
+    hours = '0' + hours
+  }
+  if (minutes < 10) {
+    minutes = '0' + minutes
+  }
+
+  const dateMonthYear = date + '.' + month + '.' + year
+  const time = hours + ':' + minutes
+  const fullTime = dateMonthYear + ' ' + time
+  return fullTime
+}
+
+// Loops
+for (let i = 0; i < n; i++){
+    console.log()
+}
+
+// declaring an array variable
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+
+// iterating an array using regular for loop
+let len = names.length;
+for(let i = 0; i < len; i++){
+    console.log(names[i].toUpperCase())
+}
+
+
+// iterating an array using for of
+for( const name of names) {
+    console.log(name.toUpperCase())
+}
+
+// iterating array using forEach 
+names.forEach((name) => name.toUpperCase())
+
+
+const person = {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+  age: 250,
+  country: 'Finland',
+  city: 'Helsinki',
+  skills: ['HTML','CSS','JavaScript','React','Node','MongoDB','Python','D3.js'],
+  isMarried: true
+}
+for(const key in person) {
+    console.log(key)
+}
+
+// Object
+// declaring object literal
+const perrson = {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+  age: 250,
+  country: 'Finland',
+  city: 'Helsinki',
+  skills: ['HTML','CSS','JavaScript','TypeScript', 'React','Node','MongoDB','Python','D3.js'],
+  isMarried: true
+}
+// iterating through object keys
+for(const key in perrson) {
+    console.log(key, perrson[key])
+}
+
+// conditional
+// if else if else if else
+
+let a = 0
+if (a > 0) {
+ console.log(`${a} is a positive number`)
+} else if (a < 0) {
+ console.log(`${a} is a negative number`)
+} else if (a == 0) {
+ console.log(`${a} is zero`)
+} else {
+ console.log(`${a} is not a number`)
+}
+
+// defining class: We declare class with CamelCase which starts with capital letter.
+class Person {
+  constructor(firstName, lastName) {
+    console.log(this) // Check the output from here
+    this.firstName = firstName
+    this.lastName = lastName
+  }
+}
+
