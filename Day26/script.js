19fd1f8d70256baed39c6105a2c2623a77dd8c5b
Hawa-codes <adediranhawa@gmail.com>
2025-09-24 11:24:57 +0100

    Day 26

---
+++ Day26/script.js
@@ -0,0 +1,15 @@
+const total = document.querySelector(".total");
+
+const searchNo = document.querySelector(".searchno");
+
+const btn1 = document.querySelector(".btn1");
+
+const btn2 = document.querySelector(".btn2");
+
+const order = document.querySelector(".order");
+
+const img = document.querySelector("arrow");
+
+const arr1 = document.querySelector(".arrowup");
+
+const arr2 = document.querySelector(".arrowdown");
